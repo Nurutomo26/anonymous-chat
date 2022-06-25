3bac1d3b6da56d9c6f83b1b470d1b1e5722237af
Nurutomo26 <103919858+Nurutomo26@users.noreply.github.com>
2022-06-25 09:21:54 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-TOKEN = os.environ.get("TOKEN", None) # Dapatkan di @botfather
-OWNER = os.environ.get("OWNER", "kenkanasw") # Isi dengan username kalian tanpa tanda @
-GROUP = os.environ.get("GROUP", "musikkugroup") # Isi dengan username group kalian tanpa tanda @ kalau gak punya gak usah isi
-CHANNEL = os.environ.get("CHANNEL", "musikkuchannel") # Isi dengan username channel kalian tanpa tanda @ kalau gak punya gak usah isi
+TOKEN = os.environ.get("TOKEN", "5401473448:AAHoa3zfOmK4Lg9uw42D_r_H6MzL4MaV5h8") # Dapatkan di @botfather
+OWNER = os.environ.get("OWNER", "clevv26") # Isi dengan username kalian tanpa tanda @
+GROUP = os.environ.get("GROUP", "clevvbotzofc") # Isi dengan username group kalian tanpa tanda @ kalau gak punya gak usah isi
+CHANNEL = os.environ.get("CHANNEL", "clevvbotzchannel") # Isi dengan username channel kalian tanpa tanda @ kalau gak punya gak usah isi
